4de1d090c270643bd8faab3167bff2322d32efad
tikmaram prajapati <tikmaramprajapati10b@gmail.com>
2025-09-29 16:14:36 +0530

    added content and footer

--- app/landingPage/landingpage.tsx
+++ app/landingPage/landingpage.tsx
@@ -1,10 +1,14 @@
 import Navbar from "@/components/Navbar"
 import HeroSection from "@/components/HeroSection"
+import Content from "@/components/Content"
+import Footer from "@/components/Footer"
 export default function Page() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <Content />
+      <Footer />
     </div>
   )
 }
